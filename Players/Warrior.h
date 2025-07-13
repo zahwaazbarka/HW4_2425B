e2b4ffd16161c112a82108d2a61d32985b68bb38
ward khleif <w4rdkh@gmail.com>
2025-07-13 16:44:02 +0300

    add warrior character

---
+++ Players/Warrior.h
@@ -0,0 +1,18 @@
+
+#include "Job.h"
+#include "Player.h"
+
+class Warrior :public Job{
+public:
+	int getCombatPower(const Player& player) const override{
+		return player.getForce() * 2 + player.getLevel();
+	}
+	std::string getJobName() const override{
+		return "warrior";
+	}
+	void onScolarEclipse(Player& player) const{
+		player.subtractForce();
+	}
+
+
+};
