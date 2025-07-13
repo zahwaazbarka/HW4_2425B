4da095006bc8612ce44850fb1cfb4f5f99fd3588
ward khleif <w4rdkh@gmail.com>
2025-07-13 17:25:27 +0300

    create the responsible type

---
+++ Players/Resposible.h
@@ -0,0 +1,18 @@
+
+#pragma once 
+
+#include "CharacterType.h"
+
+class Responsible: public CharacterType{
+public:
+	int shouldBuyPotion(int currentHp,int maxHp,int coins) const override{
+		int missingHp = maxHp-currentHp;
+		int neededAmountByHealth = (missingHp/10) +1;
+		int canAfordByCoins = coins/5;
+		if(neededAmountByHealth > canAfordByCoins)
+		{
+			return canAfordByCoins;
+		}
+		return neededAmountByHealth;
+	}
+};
