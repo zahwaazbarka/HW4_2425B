5252b8a8fe247673983b64501b7d5c8f67ccc20e
ward khleif <w4rdkh@gmail.com>
2025-07-13 17:09:40 +0300

    remove the parameter in isranged

--- Players/Job.h
+++ Players/Job.h
@@ -3,6 +3,7 @@
 #include <string>
 
 class Player;
+
 class Job{
 private:
 
@@ -11,7 +12,7 @@ virtual int getCombatPower(const Player& player) const=0;
 virtual std::string getJobName () const=0;
 virtual void onScolarExlipse(const Player& player) const=0;
 
-virtual bool isRanged(const Player& player) const=0;
+virtual bool isRanged() const=0;
 
 virtual ~Job() = default;
 
