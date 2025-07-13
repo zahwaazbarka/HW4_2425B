041bf7f7569a593df79f941e9b53076131609c83
ward khleif <w4rdkh@gmail.com>
2025-07-13 20:45:46 +0300

    create class monster to manage the monsters

---
+++ Events/Monster.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Monster.h"
+#include <string>
+class Event;
+
+class Monster{
+public:
+
+virtual int getCombatPower() const=0;
+virtual int getLoot () const=0;
+virtual int getDamage () const=0;
+virtual std::string getDescription() const =0;
+
+virtual ~Monster() = default;
+
+
+};
