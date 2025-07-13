5f5865910d49ea721b80103ba403dfc85d83d0af
ward khleif <w4rdkh@gmail.com>
2025-07-13 18:53:42 +0300

    fix from private to public mistake

--- Players/Job.h
+++ Players/Job.h
@@ -5,14 +5,14 @@
 class Player;
 
 class Job{
-private:
+public:
 
 virtual int getCombatPower(const Player& player) const=0;
 
 virtual std::string getJobName () const=0;
 virtual void onScolarExlipse(const Player& player) const=0;
 
-virtual bool isRanged() const=0;
+virtual bool isRanged(const Player& player) const=0;
 
 virtual ~Job() = default;
 
