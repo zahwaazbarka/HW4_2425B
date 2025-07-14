86716fdb9fc9ba1ea65298de136a80314e9f0170
ward khleif <w4rdkh@gmail.com>
2025-07-14 20:53:12 +0300

    add the getcharactertype function that return the type

--- Players/Resposible.h
+++ Players/Resposible.h
@@ -1,12 +1,17 @@
 
 #pragma once 
 
+#include <string>
+
 #include "CharacterType.h"
 
 class Responsible: public CharacterType{
 public:
 	Responsible()= default;
 
+	std::string getCharacterType() const override{
+		return "Responsible";
+	}
 	int shouldBuyPotion(int currentHp,int maxHp,int coins) const override{
 		int missingHp = maxHp-currentHp;
 		int neededAmountByHealth = (missingHp/10) +1;
--- Players/RiskTaking.h
+++ Players/RiskTaking.h
@@ -4,7 +4,9 @@
 class RiskTaking: public CharacterType{
 public:
 	RiskTaking()= default;
-
+	std::string getCharacterType() const override{
+		return "RiskTaking";
+	}
 	int shouldBuyPotion(int currentHp,int maxHp,int coins)const override{
 		if(currentHp < 50 && coins >= 10)
 		{
