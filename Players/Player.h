a4ec9045153efba4018a769d3fa8bd1d5896f295
zahwaazbarka <zahwaazbarka@gmail.com>
2025-07-14 23:04:26 +0300

    Update Player.h

--- Players/Player.h
+++ Players/Player.h
@@ -1,39 +1,39 @@
-
 #pragma once
 
 #include <string>
-#include "Players/Jobs/Job.h"
+#include "Jobs/Job.h"
 #include "CharacterType.h"
 
 using std::string;
+
 class Event;
+
 class Player {
 public:
 
 	virtual bool hasWon() const = 0;
 	virtual bool isKnockedOut() const = 0;
 
-    virtual std::string getName() const=0;
-    virtual int getLevel() const=0;
-    virtual int getForce() const=0;
-    virtual int getHealthPoints() const=0;
-    virtual int getMaxHealthPoints() const=0;
-    virtual int getCoins() const=0;
-    virtual Job* getJob() const =0;
-    virtual CharacterType* getCharacter() const =0;
+    virtual std::string getName() const = 0;
+    virtual int getLevel() const = 0;
+    virtual int getForce() const = 0;
+    virtual int getHealthPoints() const = 0;
+    virtual int getMaxHealthPoints() const = 0;
+    virtual int getCoins() const = 0;
+    virtual Job* getJob() const = 0;
+    virtual CharacterType* getCharacter() const = 0;
 
-	virtual void addForce(int amount)=0;
-    virtual void subtractForce(int amount)=0;
+	virtual void addForce(int amount) = 0;
+    virtual void subtractForce(int amount) = 0;
 
-	virtual void heal(int amount) =0;
-	virtual void damage(int amount) =0;
-	virtual void levelUp(int amount) =0;
-	virtual void pay(int amount) =0;
-	virtual void earn(int amount) =0;
+	virtual void heal(int amount) = 0;
+	virtual void damage(int amount) = 0;
+	virtual void levelUp(int amount) = 0;
+	virtual void pay(int amount) = 0;
+	virtual void earn(int amount) = 0;
 
-    virtual void playTurn(Event* event) =0;
+    virtual void playTurn(Event* event) = 0;
 	virtual std::string getDescription() const = 0;
-
 
     virtual ~Player() = default;
 };
