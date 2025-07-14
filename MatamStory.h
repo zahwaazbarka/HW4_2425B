8228abc66cba440b74acb473c3b3a55036c1eb1d
ward khleif <w4rdkh@gmail.com>
2025-07-14 20:55:18 +0300

    add to matam story the two vectors that have shred_ptr - one have all the players and one have all the events

--- MatamStory.h
+++ MatamStory.h
@@ -1,55 +1,55 @@
-
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 #include "Players/Player.h"
 #include "Events/Event.h"
 
-
-class MatamStory{
+class MatamStory {
 private:
-    unsigned int m_turnIndex;
+ unsigned int m_turnIndex;
+ std::vector<std::shared_ptr<Player>> m_players;
+ std::vector<std::shared_ptr<Event>> m_events;
 
-    /**
-     * Playes a single turn for a player
-     *
-     * @param player - the player to play the turn for
-     *
-     * @return - void
-    */
-    void playTurn(Player& player);
+ /**
+  * Playes a single turn for a player
+  *
+  * @param player - the player to play the turn for
+  *
+  * @return - void
+ */
+ void playTurn(Player& player);
 
-    /**
-     * Plays a single round of the game
-     *
-     * @return - void
-    */
-    void playRound();
+ /**
+  * Plays a single round of the game
+  *
+  * @return - void
+ */
+ void playRound();
 
-    /**
-     * Checks if the game is over
-     *
-     * @return - true if the game is over, false otherwise
-    */
-    bool isGameOver() const;
+ /**
+  * Checks if the game is over
+  *
+  * @return - true if the game is over, false otherwise
+ */
+ bool isGameOver() const;
 
 public:
-    /**
-     * Constructor of MatamStory class
-     *
-     * @param eventsStream - events input stream (file)
-     * @param playersStream - players input stream (file)
-     *
-     * @return - MatamStory object with the given events and players
-     *
-    */
-    MatamStory(std::istream& eventsStream, std::istream& playersStream);
+ /**
+  * Constructor of MatamStory class
+  *
+  * @param eventsStream - events input stream (file)
+  * @param playersStream - players input stream (file)
+  *
+  * @return - MatamStory object with the given events and players
+ */
+ MatamStory(std::istream& eventsStream, std::istream& playersStream);
 
-    /**
-     * Plays the entire game
-     *
-     * @return - void
-    */
-    void play();
+ /**
+  * Plays the entire game
+  *
+  * @return - void
+ */
+ void play();
 };
