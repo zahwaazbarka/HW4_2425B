ccb6cb38014607f2ea61d5fb89195e9d4ed7096b
ward khleif <w4rdkh@gmail.com>
2025-07-14 20:58:09 +0300

    make the game work it reads the input split it to vecors and play entill win or all lose

--- MatamStory.cpp
+++ MatamStory.cpp
@@ -1,66 +1,132 @@
+#pragma once
 
 #include "MatamStory.h"
 
 #include "Utilities.h"
+#include "Events/MonsterEncounter.h"
+#include "Events/PotionsMerchant.h"
+#include "Events/SolarEclipse.h"
+#include "Events/Monsters/Snail.h"
+#include "Events/Monsters/Slime.h"
+#include "Events/Monsters/Balrog.h"
+#include "Events/Monsters/Pack.h"
+#include "Players/GamePlayer.h"
+#include "Players/Jobs/Warrior.h"
+#include "Players/Jobs/Magician.h"
+#include "Players/Jobs/Archer.h"
+#include "Players/Resposible.h"
+#include "Players/RiskTaking.h"
 
-MatamStory::MatamStory(std::istream& eventsStream, std::istream& playersStream) {
+#include <sstream>
+#include <stdexcept>
 
-    /*===== TODO: Open and read events file =====*/
+using namespace std;
 
-    /*==========================================*/
-
-
-    /*===== TODO: Open and Read players file =====*/
-
-    /*============================================*/
-
-
-    this->m_turnIndex = 1;
+static unique_ptr<Job> createJob(const string& name) {
+    if (name == "Warrior") return make_unique<Warrior>();
+    if (name == "Magician") return make_unique<Magician>();
+    if (name == "Archer") return make_unique<Archer>();
+    throw runtime_error("Invalid Players File");
 }
 
+static unique_ptr<CharacterType> createCharacter(const string& name) {
+    if (name == "Responsible") return make_unique<Responsible>();
+    if (name == "RiskTaking") return make_unique<RiskTaking>();
+    throw runtime_error("Invalid Players File");
+}
+
+static shared_ptr<Monster> createMonster(const string& name) {
+    if (name == "Snail") return make_shared<Snail>();
+    if (name == "Slime") return make_shared<Slime>();
+    if (name == "Balrog") return make_shared<Balrog>();
+    throw runtime_error("Invalid Events File");
+}
+
+vector<shared_ptr<Event>> parseEvents(istream& in) {
+    vector<shared_ptr<Event>> events;
+    string word;
+    while (in >> word) {
+        if (word == "Snail" || word == "Slime" || word == "Balrog") {
+            events.push_back(make_shared<MonsterEncounter>(createMonster(word)));
+        } else if (word == "SolarEclipse") {
+            events.push_back(make_shared<SolarEclipse>());
+        } else if (word == "PotionsMerchant") {
+            events.push_back(make_shared<PotionsMerchant>());
+        } else if (word == "Pack") {
+            int packSize;
+            if (!(in >> packSize)) throw runtime_error("Invalid Events File");
+            auto pack = make_shared<Pack>();
+            for (int i = 0; i < packSize; ++i) {
+                if (!(in >> word)) throw runtime_error("Invalid Events File");
+                pack->addMonster(createMonster(word));
+            }
+            events.push_back(make_shared<MonsterEncounter>(pack));
+        } else {
+            throw runtime_error("Invalid Events File");
+        }
+    }
+    if (events.size() < 2) throw runtime_error("Invalid Events File");
+    return events;
+}
+
+vector<shared_ptr<Player>> parsePlayers(istream& in) {
+    vector<shared_ptr<Player>> players;
+    string name, job, character;
+    while (in >> name >> job >> character) {
+        if (name.length() < 3 || name.length() > 15) throw runtime_error("Invalid Players File");
+        players.push_back(make_shared<GamePlayer>(name, createJob(job).release(), createCharacter(character).release()));
+    }
+    if (players.size() < 2 || players.size() > 6) throw runtime_error("Invalid Players File");
+    return players;
+}
+
+MatamStory::MatamStory(istream& eventsStream, istream& playersStream)
+    : m_turnIndex(1), m_events(parseEvents(eventsStream)), m_players(parsePlayers(playersStream)) {}
+
 void MatamStory::playTurn(Player& player) {
-
-    /**
-     * Steps to implement (there may be more, depending on your design):
-     * 1. Get the next event from the events list
-     * 2. Print the turn details with "printTurnDetails"
-     * 3. Play the event
-     * 4. Print the turn outcome with "printTurnOutcome"
-    */
-
-    m_turnIndex++;
+    Event& event = *m_events[(m_turnIndex - 1) % m_events.size()];
+    printTurnDetails(m_turnIndex, player, event);
+    string outcome = event.apply(&player);
+    printTurnOutcome(outcome);
+    ++m_turnIndex;
 }
 
 void MatamStory::playRound() {
-
     printRoundStart();
-
-    /*===== TODO: Play a turn for each player =====*/
-
-    /*=============================================*/
-
+    for (auto& p : m_players) {
+        if (!p->isKnockedOut() && !p->hasWon()) {
+            playTurn(*p);
+        }
+    }
     printRoundEnd();
-
     printLeaderBoardMessage();
 
-    /*===== TODO: Print leaderboard entry for each player using "printLeaderBoardEntry" =====*/
+    sort(m_players.begin(), m_players.end(), [](const shared_ptr<Player>& a, const shared_ptr<Player>& b) {
+        if (a->getLevel() != b->getLevel()) return a->getLevel() > b->getLevel();
+        if (a->getCoins() != b->getCoins()) return a->getCoins() > b->getCoins();
+        return a->getName() < b->getName();
+    });
 
-    /*=======================================================================================*/
-
+    for (size_t i = 0; i < m_players.size(); ++i) {
+        printLeaderBoardEntry(i + 1, *m_players[i]);
+    }
     printBarrier();
 }
 
 bool MatamStory::isGameOver() const {
-    /*===== TODO: Implement the game over condition =====*/
-    return false; // Replace this line
-    /*===================================================*/
+    for (const auto& p : m_players) {
+        if (!p->isKnockedOut() && !p->hasWon()) {
+            return false;
+        }
+    }
+    return true;
 }
 
 void MatamStory::play() {
     printStartMessage();
-    /*===== TODO: Print start message entry for each player using "printStartPlayerEntry" =====*/
-
-    /*=========================================================================================*/
+    for (size_t i = 0; i < m_players.size(); ++i) {
+        printStartPlayerEntry(i + 1, *m_players[i]);
+    }
     printBarrier();
 
     while (!isGameOver()) {
@@ -68,7 +134,14 @@ void MatamStory::play() {
     }
 
     printGameOver();
-    /*===== TODO: Print either a "winner" message or "no winner" message =====*/
-
-    /*========================================================================*/
+    bool winner = false;
+    for (const auto& p : m_players) {
+        if (p->hasWon()) {
+            printWinner(*p);
+            winner = true;
+        }
+    }
+    if (!winner) {
+        printNoWinners();
+    }
 }
